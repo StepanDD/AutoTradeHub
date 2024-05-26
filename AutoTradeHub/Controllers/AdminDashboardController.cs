bce83b054b00abf4b93239b32ecc758151095fd5
Степан Дубинин <Stepdu90@mail.ru>
2024-05-26 20:20:52 +1000

    Admin base dashboard

--- AutoTradeHub/Controllers/AdminDashboardController.cs
+++ AutoTradeHub/Controllers/AdminDashboardController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoTradeHub.Interfaces;
+using AutoTradeHub.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AutoTradeHub.Controllers
@@ -6,9 +8,35 @@ namespace AutoTradeHub.Controllers
 	[Authorize(Roles = "Admin")]
 	public class AdminDashboardController : Controller
 	{
-		public IActionResult Index()
+		private readonly IUserRepository _userRepository;
+
+		public AdminDashboardController(IUserRepository userRepository)
+        {
+			_userRepository = userRepository;
+		}
+        public async Task<IActionResult> Index()
 		{
-			return View();
+			var curUser = await _userRepository.GetCurrentUser();
+			var user = new UserVM(curUser);
+			return View(user);
+		}
+		public async Task<IActionResult> Users()
+		{
+			var curUser = await _userRepository.GetCurrentUser();
+			var user = new UserVM(curUser);
+			return View(user);
+		}
+		public async Task<IActionResult> MMG()
+		{
+			var curUser = await _userRepository.GetCurrentUser();
+			var user = new UserVM(curUser);
+			return View(user);
+		}
+		public async Task<IActionResult> Colors()
+		{
+			var curUser = await _userRepository.GetCurrentUser();
+			var user = new UserVM(curUser);
+			return View(user);
 		}
 	}
 }
